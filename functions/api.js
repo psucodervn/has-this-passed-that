f2dad2e736b6a68c1f8d4d45ebf7a3f964495a22
Hưng Lê <hungle.info@gmail.com>
2019-08-17 16:36:55 +0700

    remove log

--- functions/api.js
+++ functions/api.js
@@ -1,14 +1,12 @@
 const fetch = require('node-fetch')
 const API_KEY = process.env.YOUTUBE_API_KEY;
-console.log(API_KEY);
 
 const API_ENDPOINT = `https://www.googleapis.com/youtube/v3/videos?part=snippet,statistics&id=knW7-x7Y7RE%2Cj8U06veqxdU&key=${API_KEY}`
 
 exports.handler = async (event, context) => {
   let data
   try {
-    data = await fetch(API_ENDPOINT).then(res => res.json())
-    console.log(data);
+    data = await fetch(API_ENDPOINT).then(res => res.json());
     // handle response
     return {
       statusCode: 200,
